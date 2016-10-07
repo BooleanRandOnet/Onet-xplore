fb907e7b277ebf74a5b9feb31d58b60615fde827
hugo-clems <hugo.clems@outlook.fr>
2016-10-07 17:24:19 +0200

    Test

--- app/src/main/java/booleancorp/onetxplore/MainActivity.java
+++ app/src/main/java/booleancorp/onetxplore/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Ceci est un test GitHub
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

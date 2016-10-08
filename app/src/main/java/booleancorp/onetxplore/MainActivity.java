24370adbed6104f077e1920f709e13d5f2929de6
hugo-clems <hugo.clems@outlook.fr>
2016-10-08 14:20:54 +0200

    bonjour

--- app/src/main/java/booleancorp/onetxplore/MainActivity.java
+++ app/src/main/java/booleancorp/onetxplore/MainActivity.java
@@ -1,11 +1,12 @@
 package booleancorp.onetxplore;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    // Test à marché !!
+    // Test à marché !! cc
+    // OUI JE FAIS DES CHANGEMENTS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

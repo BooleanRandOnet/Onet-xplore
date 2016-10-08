945c28d19f181430389f7fa765106dea0705c5a2
hugo-clems <hugo.clems@outlook.fr>
2016-10-08 15:48:47 +0200

    rien qui marche

---
+++ app/src/main/java/booleancorp/onetxplore/Constante.java
@@ -0,0 +1,11 @@
+package booleancorp.onetxplore;
+
+import android.util.DisplayMetrics;
+
+public class Constante {
+    DisplayMetrics metrics = new DisplayMetrics();
+    getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+    int screenWidth = metrics.widthPixels;
+    int screenHeight = metrics.heightPixels;
+}

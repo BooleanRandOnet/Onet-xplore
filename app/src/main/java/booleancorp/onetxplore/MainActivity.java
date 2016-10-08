c125355888299b77b4e0b665deb0ff7757c0ba58
Guillaume <G.Cruwp@gmail.com>
2016-10-08 14:38:04 +0200

    No title bar

--- app/src/main/java/booleancorp/onetxplore/MainActivity.java
+++ app/src/main/java/booleancorp/onetxplore/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Cacher la barre du titre
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }

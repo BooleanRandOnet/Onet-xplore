17e5a6afd8ddf701c425bd163572edac18629f4e
matthieubravo <matthieubravo12@gmail.com>
2017-01-17 22:38:55 +0100

    package

---
+++ app/src/main/java/booleancorp/onetxplore/view/MapView.java
@@ -0,0 +1,8 @@
+package booleancorp.onetxplore.view;
+
+/**
+ * Created by matthieubravo on 17/01/2017.
+ */
+
+public class MapView {
+}
---
+++ app/src/main/java/booleancorp/onetxplore/view/map/missions/MissionsView.java
@@ -0,0 +1,8 @@
+package booleancorp.onetxplore.view.map.missions;
+
+/**
+ * Created by matthieubravo on 17/01/2017.
+ */
+
+public class MissionsView {
+}

4883008ae448d2ace95e333fc59851fe4fdaf8b9
Swift4Evr <ideviceaddict@gmail.com>
2017-01-22 22:13:01 +0100

    Rectification des couleurs de fonds des champs lors des erreurs

--- app/src/main/java/booleancorp/onetxplore/controller/MainActivity.java
+++ app/src/main/java/booleancorp/onetxplore/controller/MainActivity.java
@@ -2,6 +2,7 @@ package booleancorp.onetxplore.controller;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
@@ -236,21 +237,8 @@ public class MainActivity extends Activity implements View.OnTouchListener{
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
         }
 
-        ////////////CACHER LA BARRE DE NAVIGATION ET DE NOTIFICATION
-        View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
-        decorView.setSystemUiVisibility(uiOptions);
-        /////////////
+        hideComponent();
     }
 
     /**
@@ -296,6 +284,9 @@ public class MainActivity extends Activity implements View.OnTouchListener{
      */
     public void clicChangeStatu(View view) {
         createContent((int)actionBT.getTag());
+        //enlever le fait que l'utilisateur s'est trompé lors de la co ()si il s'est trompé
+        etPseudo.setBackgroundResource(R.drawable.inputshape);
+        etMdp.setBackgroundResource(R.drawable.inputshape);
     }
 
     /**
@@ -378,5 +369,27 @@ public class MainActivity extends Activity implements View.OnTouchListener{
                 break;
         }
         return false;
+    }
+
+
+    /////////////////////////////////
+
+    /**
+     * Cacher les barres d'interface androïd
+     */
+    private void hideComponent(){
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
--- app/src/main/java/booleancorp/onetxplore/view/map/MapsActivity.java
+++ app/src/main/java/booleancorp/onetxplore/view/map/MapsActivity.java
@@ -5,6 +5,7 @@ import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 
 import com.google.android.gms.location.LocationListener;
@@ -36,20 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //location manager
         UserLocationManager userLocationManager = new UserLocationManager();
 
-        View decorView = getWindow().getDecorView();
-        // Hide both the navigation bar and the status bar.
-        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-        // a general rule, you should design your app to hide the status bar whenever you
-        // hide the navigation bar.
-        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-        decorView.setSystemUiVisibility(uiOptions);
-        /////////////
+        hideComponent();
     }
 
 
@@ -71,5 +59,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
+    }
+
+
+    /////////////////////////////////
+
+    /**
+     * Cacher les barres d'interface androïd
+     */
+    private void hideComponent(){
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }

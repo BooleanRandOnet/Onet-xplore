bc40b932f336a2e38b72bb8913d4b18de05f9c7b
MaxAveyroApps <maxapps.dev@gmail.com>
2017-01-26 21:36:09 +0100

    Travail sur la bottom bar

--- app/src/main/java/booleancorp/onetxplore/tools/UserLocationManager.java
+++ app/src/main/java/booleancorp/onetxplore/tools/UserLocationManager.java
@@ -11,7 +11,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
